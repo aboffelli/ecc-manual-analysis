5057a2cb2ded9b937cc14f2d35306094ede79efc
Arthur Boffelli Castro <70641295+aboffelli@users.noreply.github.com>
2025-03-21 22:23:41 +0100

    Removed paired tumor lines
    - Removed everything from the paired tumor, since that is not necessary anymore.
    - Added more detailed comments.

--- patient_data_parsers/patient_data_parser.py
+++ patient_data_parsers/patient_data_parser.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+# Set the path for the files.
 patient_file = ('/home/aboffelli/PACC/TMA/Data/Tables/'
                 'SB91RT_final_20170821_PrimoLR_EjPnr_orginal.csv')
 tma_map = '/home/aboffelli/PACC/TMA/Data/Tables/Koppling_PathXL_till_TMA-nr.csv'
@@ -12,104 +13,100 @@ print(patient_df.head(10))
 tma_df = pd.read_csv(tma_map, delimiter='\t').dropna()
 tma_df['TMAnr'] = tma_df['TMAnr'].astype(int)
 
-patient_df['PACC'] = None; print(patient_df.head(10))
-patient_df['tma_nr'] = patient_df['tma_nr'].replace('#NULL!', 0); print(patient_df.head(10))
+patient_df['PACC'] = None  # Create a column for the ECC score
+
+# Replace the #NULL! to 0 to avoid errors when transforming it into float.
+patient_df['tma_nr'] = patient_df['tma_nr'].replace('#NULL!', 0)  
 patient_df['tma_nr'] = patient_df['tma_nr'].astype(float)
 print(patient_df.head(10))
 
+# Merge the tma map and patient table based on the TMA number.
 result = pd.merge(tma_df, patient_df, right_on='tma_nr', left_on='TMAnr', 
-                  how='left'); print(result.sort_values('patnr').head(20))
+                  how='left')
 
 # Get the PACC score for each core
 glas_scores = {}
 with open("/home/aboffelli/PACC/TMA/Results/ManualScoreTables/complete_patho_results.csv", 
           "r") as scores:
-    header = scores.readline()
+    header = scores.readline()  # read the header
     for line in scores:
         line_split=line.strip('\n').split('\t')
-        glas,core,pacc= line_split[0:3]
+        glas,core,pacc= line_split[0:3]  # Save the important info in variables to easy access
 
         if pacc:
             if len(pacc)>1:
+              """
+              Since the pathologists filled the table, sometimes there is text together 
+              with the score, e.g. "2?", or "1 good".
+              So we try to extract the first caracter and transform it into int.
+              Since we can't account for all the possibilities, if the first character is not a number,
+              we treat it as an NA.
+              """
                 try:
                     pacc = int(pacc[0])
                 except ValueError:
                     pacc= 'NA'
             else:
-                pacc = int(pacc)
+                pacc = int(pacc)  # If there is only one character, set that as an ECC score.
         else: 
-            pacc = 'NA'
-        
+            pacc = 'NA'  # If the variable is empty, put it as an NA.
+
+        # Get the TMA dictonary or create a new entry if it doesn't exist.
         glas_scores[glas] = glas_scores.get(glas, {})
         
-        # Considering PACC score
+        # Add the ECC score to the dictionary
         glas_scores[glas][core] = pacc
         
-# Put the PACC score in the table
+# Add the ECC score in the table
 result["PACC"] = None
 
 for index, row in result.iterrows():
     tma = row['PARENT']
     core = row['ID']
-    score = glas_scores.get(tma, {}).get(core, 'NA')
-    result.at[index, 'PACC'] = score
+    score = glas_scores.get(tma, {}).get(core, 'NA')  # Retrieve the ECC score from the dictionary.
+    result.at[index, 'PACC'] = score  # add to the dataframe.
 
+# Save the file. This file contain the TMA numbers and duplicated patients (2 cores per patient).
 result.sort_values(by='patnr').to_csv('/home/aboffelli/PACC/TMA/Results/all_patients_with_TMA_PACC_new.csv',
                                       index=False, sep='\t')
-paired_samples = result[result['tumor'].isin(["1.00","2.00"])]
 
-#result = result[result['tumor'] == '1.00']
-
-# Merge the cores and scores for the same patients.
-print(result)
-df_unique = result.drop_duplicates(subset=['patnr','tumor']); print(df_unique) 
+# Remove duplicate entries based on patient ID ('patnr') and tumor type ('tumor')
+df_unique = result.drop_duplicates(subset=['patnr','tumor'])
+# Drop unnecessary columns
 df_unique = df_unique.drop(['TMAnr', 'PARENT','ID', 'PACC'], axis=1)      
 
+# Convert the ECC score to numeric values, replacing errors with -1 to indicate missing or invalid data
+# We need it as a number to calculate the max.
 result['PACC'] = pd.to_numeric(result['PACC'], errors='coerce').fillna(-1)
+# Aggregate ECC score by taking the maximum value per patient ('patnr')
 pacc_agg = result.groupby('patnr')['PACC'].max().reset_index()
 
+# Merge the unique patient data with the aggregated ECC scores
 result_unique = pd.merge(df_unique, pacc_agg, on='patnr', 
-                         how='left').sort_values(by='patnr'); print(result_unique)
-#result_unique.to_csv('/home/aboffelli/TMA/Results/all_patient_unique_with_pacc_full_score.csv', 
-#                     index=False, sep='\t')
+                         how='left').sort_values(by='patnr')
 
-# Merge the cores and score for the paired sample patients.
-df_grouped = paired_samples.groupby('patnr')['tumor'].nunique()
-patients_with_both_types = df_grouped[df_grouped == 2].index
-
-
-result_paired = paired_samples[paired_samples['patnr'].isin(patients_with_both_types)]
-
-df_unique = result_paired.drop_duplicates(subset=['patnr','tumor'])
-
-df_unique = df_unique.drop(['TMAnr', 'PARENT','ID', 'PACC'], axis=1)
-
-result_paired['PACC'] = pd.to_numeric(result_paired['PACC'], 
-                                      errors='coerce').fillna(-1).astype('Int64')
-pacc_agg = result_paired.groupby(['patnr','tumor'])['PACC'].max().reset_index()
-result_paired_unique = pd.merge(df_unique, pacc_agg, on=['patnr', 'tumor'], 
-                        how='left').sort_values(by='patnr')
-#result_paired_unique.to_csv('/home/aboffelli/TMA/Results/paired_patient_unique_with_pacc_full_score.csv', 
- #                    index=False, sep='\t')
-
-
-
-
+# Identify patients present in 'patient_df' but missing from 'result_unique'
 differing_values = patient_df[~patient_df['patnr'].isin(result_unique['patnr'])]
 
-print(differing_values)
+# Select patients who do not have TMA data
+no_tma = patient_df[patient_df['tma_nr'] == 0]
 
-no_tma = patient_df[patient_df['tma_nr'] == 0]; print(no_tma)
-tumor_three = patient_df[patient_df['tumor'] == "3.00"]; print(tumor_three)
+# Select patients with tumor type labeled as "3.00"
+tumor_three = patient_df[patient_df['tumor'] == "3.00"]
+
+# Combine both groups and remove duplicates
 to_add = pd.concat([no_tma, tumor_three], ignore_index=True).drop_duplicates(subset=['patnr','tumor'])
-print(to_add)
 
-result_unique = pd.concat([result_unique, to_add], 
-                          ignore_index=True).drop_duplicates(subset=['patnr','tumor']).sort_values('patnr')
+
+# Append missing patients back into the dataset
+result_unique = pd.concat([result_unique, to_add], ignore_index=True)
+
+# Remove any duplicates and ensure the dataset is sorted by 'patnr'
+result_unique = result_unique.drop_duplicates(subset=['patnr','tumor']).sort_values('patnr')
+
+# Change the -1 values back to NA.
 result_unique['PACC'] = result_unique['PACC'].replace(-1, 'NA').fillna('NA')
-print(result_unique.head(10))
 
+# Save the final dataframe to a file
 result_unique.to_csv('/home/aboffelli/PACC/TMA/Results/all_patient_unique_with_pacc_full_score_new.csv', 
                      index=False, sep='\t')
-
-# print(patient_df)
