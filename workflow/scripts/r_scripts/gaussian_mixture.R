8dc2c26de44f07b859980beb4f7e2834fafb8b9f
aboffelli <arthur.boffelli@outlook.com>
2025-08-26 16:31:27 +0200

    Fixed the 99percentile problem that arised with the merging
    Since the python script considered a 99 percentile for the gmm file, the R script also had to be changed to generate the right file.

--- workflow/scripts/r_scripts/gaussian_mixture.R
+++ workflow/scripts/r_scripts/gaussian_mixture.R
@@ -11,10 +11,17 @@ suppressMessages(suppressWarnings({
 
     plot_gmm_histogram <- function(data, image_name, filename) {
         filename = str_remove(filename, "_measurements.txt")
-        #cat(paste("Processed: ", filename, image_name, "\n"))
-        gmm <- Mclust(data$`Area µm^2`, G = 2)
         
-
+        gmm <- Mclust(data$`Area µm^2`, G = 2)  # calculate the Gaussian mixture
+    
+        # Calculate the percentiles for the Area.
+        quartiles <- quantile(data$`Area µm^2`, probs = c(0.25, 0.50, 0.75, 0.99))
+        q1 <- quartiles[1]  # 25th percentile
+        median <- quartiles[2]  # 50th percentile (median)
+        q3 <- quartiles[3]  # 75th percentile
+        cutoff_99 <- quartiles[4]  # 99th percentile
+        
+        # Extract the means, variance, and proportion from the gm model.
         means <- gmm$parameters$mean
         variances <- gmm$parameters$variance$sigmasq
         proportions <- gmm$parameters$pro
@@ -38,33 +45,71 @@ suppressMessages(suppressWarnings({
         
 
         p <- data %>% 
-            ggplot(aes(`Area µm^2`)) +
-            geom_histogram(aes(y = after_stat(density)), bins = 300, 
-                        fill = "#619CFF", color = "#08519C", alpha=0.5) +
-            geom_line(data = density_data, aes(x = x, y = y), color = "black",
-                    linewidth = 1.2) +
-            geom_vline(xintercept = means, linetype = c("dashed", "dotdash"), ,
-                    color = c("#A50026", "#E08214"), size = 2) +
-            ggtitle(image_name) +
-            labs(x="Nuclear Area", y="Density") +
-            theme_classic(base_size = 20)+
-            xlim(0,1000)
+        ggplot(aes(`Area µm^2`)) +
         
-        print(p)
+        # Histogram for the area
+        geom_histogram(aes(y = after_stat(density)), bins = 300, 
+                       fill = "#619CFF", color = "#08519C", alpha=0.5) +
+        
+        # Line for the GMM fit
+        geom_line(data = density_data, aes(x = x, y = y), color = "black",
+                  linewidth = 1.2) +
+        
+        ## Add the quartiles lines (maybe not necessary)
+        # geom_vline(xintercept = c(q1, median, q3), linetype = "dotted", 
+        #            color = "darkgray", linewidth = 1) +  # Quartiles
+        
+        # Add the 99th percentile cutoff
+        geom_vline(xintercept = cutoff_99, linetype = "dashed", 
+                   color = "blue", linewidth = 1) +
+        
+        # Add the GMM mean lines for 1st and 2nd components.
+        geom_vline(xintercept = means, linetype = c("dashed", "dotdash"), ,
+                   color = c("#A50026", "#E08214"), size = 1) +
+        
+        ## Annotate the quartile lines.
+        # annotate("text", x = c(q1, median, q3), y = 0.025, 
+        #          label = c("Q1", "Median", "Q3"), angle = 90, 
+        #          vjust = -0.5, size = 4, color = "darkgray") +
+        
+        # Annotate the 99th percentile line
+        annotate("text", x = cutoff_99, y = 0.028, label = "99%", angle = 90, 
+                 vjust = -0.5, size = 4, color = "blue") +
+        
+        # Annotate the GMM component lines
+        annotate("text", x = means, y = 0.026, 
+                 label = c("1st comp mean", "2nd comp mean"), angle = 90, 
+                 vjust = -0.5, size = 4, color = c("#A50026", "#E08214")) +
+        
+        # Edit the title and labels
+        labs(title = image_name,
+             x = "Nuclear Area (µm²)",
+             y = "Density") +
+        
+        # clean theme
+        theme_classic(base_size = 20)+
+        
+        # Increase the X axis limits
+        xlim(0,1000)
+    
+    # Print the plot in the pdf.
+    print(p)
 
-        # Return summary statistics as a tibble
-        tibble(
-            Parent = filename,
-            Image = image_name,
-            mean_cp1 = means[1],
-            mean_cp2 = means[2],
-            variation_cp1 = variances[1],
-            variation_cp2 = variances[2],
-            proportion_cp1 = proportions[1],
-            proportion_cp2 = proportions[2]
-        )
+    # Return summary statistics as a tibble
+    tibble(
+        Parent = filename,
+        Image = image_name,
+        mean_cp1 = means[1],
+        mean_cp2 = means[2],
+        variation_cp1 = variances[1],
+        variation_cp2 = variances[2],
+        proportion_cp1 = proportions[1],
+        proportion_cp2 = proportions[2],
+        percentile_99 = cutoff_99,
+        fc_99 = cutoff_99/means[2]
+    )
 
-    }
+}
 
     # Access the input files
     filenames <- snakemake@input
